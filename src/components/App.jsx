a7ce33c60761fed548dacb64fe09370a641ecca7
helenserb <serbinenkoifbb@gmail.com>
2023-03-26 19:43:39 -0400

    step 1 + step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,57 @@
 import { Component } from "react";
-import { Container } from "./Container/Container";
+import { nanoid } from 'nanoid';
 import Form from "./Form/Form";
+import { ContactList } from "./ContactList/ContactList";
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
- 
   };
 
+  // createContact = data => {
+  //     this.setState(() => {
+  //       if (
+  //         this.state.contacts.find(
+  //           contact => contact.name.toLowerCase() === data.name.toLowerCase()
+  //         )
+  //       ) {
+  //         alert(`${data.name} is alredy in contacts`);
+  //       } else {
+  //         return {
+  //           contacts: [
+  //             { id: nanoid(), name: data.name, number: data.number },
+  //             ...this.state.contacts,
+  //           ],
+  //         };
+  //       }
+  //     });
+  //   };
 
-
+  addContact = data => {
+    this.setState(() => {
+      return {
+        contacts: [
+          ...this.state.contacts,
+          { id: nanoid(), name: data.name, number: data.number },
+        ],
+      };
+    });
+  };
 
   render = () => {
     return (
-   <Container>
+      <>
         <h1>Phonebook</h1>
-          <Form/>
-          <h2>Contacts</h2>
-          <ul></ul>
-   </Container>
+        <Form onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <ContactList contacts={this.state.contacts}/>
+      </>
     );
   };
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import { nanoid } from 'nanoid'
+
 
 export default class Form extends Component {
   state = {
@@ -6,32 +8,54 @@ export default class Form extends Component {
     number: '',
   };
 
+  nameInputId = nanoid();
+  numberInputId = nanoid();
+
   handleChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({ [name]: value });
   };
-    
+
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
   };
 
   render = () => {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label htmlFor="id">
-          Name
-          <input
-            id="id"
-            type="text"
-            name="name"
-            value={this.state.name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-          />
-        </label>
+        <label htmlFor={this.nameInputId}>Name</label>
+        <input
+          id={this.nameInputId}
+          type="text"
+          name="name"
+          value={this.state.name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.handleChange}
+        />
+
+        <label htmlFor={this.numberInputId}>Number</label>
+        <input
+          id={this.numberInputId}
+          type="tel"
+          name="number"
+          value={this.state.number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={this.handleChange}
+        />
+
         <button type="submit">Add contact</button>
       </form>
     );
