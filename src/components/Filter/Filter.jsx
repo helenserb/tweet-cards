24f3e76b625c1bbc165158971a8f072947afa9af
helenserb <serbinenkoifbb@gmail.com>
2023-03-26 21:09:06 -0400

    filter styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,13 @@
 import { nanoid } from 'nanoid';
+import css from './Filter.module.css'
 
-export const Filter = ({value, onChange}) => {
+export const Filter = ({onChange}) => {
     const filterId = nanoid();
   return (
-    <div>
-      <label htmlFor={filterId}>Find contacts by name:</label>
-          <input id={filterId} type="text"  onChange={onChange} name = "filter"/>
+    <div className={css.filter} >
+      <label htmlFor={filterId}>Find contacts by name: </label>
+      <input
+       className={css.filterInput} id={filterId} type="text" onChange={onChange} name="filter" />
     </div>
   );
 };
