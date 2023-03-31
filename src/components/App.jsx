5e108412dd6560453a46ba239d3437dfd4844078
helenserb <serbinenkoifbb@gmail.com>
2023-03-30 21:23:59 -0400

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,20 +16,20 @@ class App extends Component {
     filter: '',
   };
 
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-  //   if (parsedContacts) {
-  //     this.setState({ contacts: parsedContacts });
-  //   }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
       
-  // }
+  }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   
   addContact = data => {
     const isExist = this.state.contacts.find(
