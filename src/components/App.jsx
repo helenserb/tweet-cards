a9abeb2e45726f6206bc84c00e41b971fb316dc3
helenserb <serbinenkoifbb@gmail.com>
2023-04-04 22:57:10 -0400

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { nanoid } from 'nanoid';
-import Form from "./Form/Form";
+import { Form } from "./Form/Form";
 import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,78 +1,83 @@
-import { Component } from "react";
+import { useState } from "react";
 import { nanoid } from 'nanoid'
 import PropTypes from 'prop-types';
 import css from './Form.module.css'
 
 
-export default class Form extends Component {
-  state = {
-    name: '',
-    number: '',
+export const Form = ({ addContact }) => {
+  console.log(addContact)
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('');
+
+const nameInputId = nanoid();
+const  numberInputId = nanoid();
+
+
+const handleChange = ({target: {name, value}}) => {
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        console.log('This field does not exist');
+            break;
+    }
+
   };
 
-  nameInputId = nanoid();
-  numberInputId = nanoid();
-
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+const reset = () => {
+    setNumber('');
+    setName('');
   };
 
-  handleSubmit = e => {
+const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+
+    const newContact = addContact({ name, number });
+    newContact && reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
-  };
-
-  render = () => {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId} className={css.formLabel}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor={nameInputId} className={css.formLabel}>
           Name:
         </label>
         <input
           className={css.formInput}
-          id={this.nameInputId}
+          id={nameInputId}
           type="text"
           name="name"
-          value={this.state.name}
+          value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          onChange={this.handleChange}
+          onChange={handleChange}
         />
 
-        <label htmlFor={this.numberInputId} className={css.formLabel}>
+        <label htmlFor={numberInputId} className={css.formLabel}>
           Number:
         </label>
         <input
           className={css.formInput}
-          id={this.numberInputId}
+          id={numberInputId}
           type="tel"
           name="number"
-          value={this.state.number}
+          value={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          onChange={this.handleChange}
+          onChange={handleChange}
         />
 
         <button className={css.submitBtn} type="submit">Add contact</button>
       </form>
     );
   };
-}
+
 
 Form.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  id: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func,
 };
