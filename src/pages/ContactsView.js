c9ce37ec85d5a45017c630d22ef401e664263806
helenserb <serbinenkoifbb@gmail.com>
2023-05-06 19:25:21 -0400

    +

--- src/pages/ContactsView.js
+++ src/pages/ContactsView.js
@@ -3,10 +3,13 @@ import { Filter } from '../components/Filter/Filter';
 import { ContactList } from '../components/ContactList/ContactList'
 import { useSelector } from 'react-redux';
 import Loader from '../components/Loader/Loader';
+import { selectIsLoggedIn } from 'redux/auth/selectors';
+import { getIsLoading } from 'redux/selectors';
 
 
 export default function ContactsView() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoading = useSelector(getIsLoading);
 
   return (
     <>
@@ -16,7 +19,7 @@ export default function ContactsView() {
           <Form />
           <h2>Contacts</h2>
           <Filter />
-          {contacts && <ContactList />}
+          <ContactList />
           {isLoading && <Loader />}
         </div>
       ) : (
