fef5b32b8f19ba491ff020925ae4f8952b120a70
helenserb <serbinenkoifbb@gmail.com>
2023-05-06 20:47:28 -0400

    register

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,8 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser, selectIsLoggedIn } from 'redux/auth/selectors';
+import { logOut } from 'redux/auth/auth-operations';
+
 export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
-   <div>
-  <p>mango@mail.com</p>
-  <button>Logout</button>
-</div>
+    <>
+      {isLoggedIn ? (
+        <div>
+          <p>Welcome, {user}</p>
+          <button
+            type="button"
+            onClick={() => dispatch(logOut())}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 };
